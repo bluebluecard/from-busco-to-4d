759485ce3c590cf761899f2521e330b8709e3a67
Carvin Chen <chenjiawei201806@outlook.com>
2019-09-29 14:59:36 +0800

    Update extract_4d.py

--- extract_4d.py
+++ extract_4d.py
@@ -1,7 +1,6 @@
 import sys
 import numpy as np
-from datetime import datetime
-print(datetime.now())
+
 filePath = sys.argv[1]
 
 codonDict = {'CTT':'L','CTC':'L','CTA':'L','CTG':'L','GTT':'V','GTC':'V','GTG':'V','GTA':'V', \
@@ -16,25 +15,25 @@ with open(filePath,'r') as f:
         info = line.strip().split()
         seqList.append(list(info[1]))
         speList.append(info[0])
-print(datetime.now())
+
 seqArray = np.array(seqList)
 seqLength = seqArray.shape[1]
 if seqLength%3 != 0:
     print("The length of sequences is not a multiple of 3!")
     sys.exit()
-print(datetime.now())
+
 stackBool = np.stack((seqArray == 'A', seqArray == 'G', seqArray == 'C', seqArray == 'T', seqArray == '-'))   
-print(datetime.now())
+
 boolAGCT =  np.sum(np.any(stackBool, axis = 1), axis = 0)
-print(datetime.now())
+
 cleanPos = np.where(boolAGCT == 1)[0]
-print(datetime.now())
+
 BothTrueBase = np.stack((cleanPos[:-1] % 3 == 0,cleanPos[1:] %3 ==1))
-print(datetime.now())
+
 preCodonPos = cleanPos[np.where(np.all(BothTrueBase,axis = 0))[0]]
-print(datetime.now())
+
 codonSelect = seqArray[:,[preCodonPos,preCodonPos+1,preCodonPos+2]]
-print(datetime.now())
+
 
 temp = []
 for i in range(codonSelect.shape[2]):
@@ -46,7 +45,7 @@ for i in range(codonSelect.shape[2]):
             break
     if flag:
         temp.append(i)
-print(datetime.now())
+
 a = seqArray[:,preCodonPos[temp]+2]
 a[a == '0']='-'
 print('\t'+str(len(speList))+'\t'+str(len(temp)))
